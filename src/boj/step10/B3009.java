6dc135cf2f031259ce9c7bfe12fe7fd3c651ff38
coJC <dlwhdcks34@naver.com>
2023-08-22 22:38:35 +0900

    BOJ STEP10 3009

--- src/boj/step10/B3009.java
+++ src/boj/step10/B3009.java
@@ -3,21 +3,25 @@ package boj.step10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class B3009 {
-    static int[][] arr;
+    static List<Integer> xList;
+    static List<Integer> yList;
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        arr = new int[3][2];
+        xList = new ArrayList<Integer>();
+        yList = new ArrayList<Integer>();
 
-        for (int i=0; i< arr.length; i++) {
+        for (int i=0; i<3; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j=0; j<arr[i].length; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            xList.add(Integer.parseInt(st.nextToken()));
+            yList.add(Integer.parseInt(st.nextToken()));
         }
     }
 
@@ -27,6 +31,18 @@ public class B3009 {
     }
 
     static void solution() {
+        int xPoint = -1;
+        int yPoint = -1;
+        for (int i=0; i<3; i++) {
+            if(Collections.frequency(xList, xList.get(i)) == 1) {
+                xPoint = xList.get(i);
+            }
 
+            if(Collections.frequency(yList, yList.get(i)) == 1) {
+                yPoint = yList.get(i);
+            }
+        }
+
+        System.out.println(xPoint+" "+yPoint);
     }
 }
