b68b360e72c053892eb3192ed9486754c9f2bf78
coJC <dlwhdcks34@naver.com>
2023-08-26 10:45:39 +0900

    BOJ STEP10 5073

---
+++ src/boj/step10/B5073.java
@@ -0,0 +1,43 @@
+package boj.step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B5073 {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        while (arr[0] != 0 && arr[1] != 0 && arr[2] != 0) {
+            if (arr[2] >= arr[0] + arr[1]) {
+                System.out.println("Invalid");
+            } else if (arr[0] == arr[1] && arr[1] == arr[2]) {
+                System.out.println("Equilateral");
+            } else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[0] == arr[2]) {
+                System.out.println("Isosceles");
+            }else {
+                System.out.println("Scalene");
+            }
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+
+            Arrays.sort(arr);
+        }
+
+
+
+    }
+}
