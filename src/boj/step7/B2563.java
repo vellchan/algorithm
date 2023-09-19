bfeb939a55b5c33c87e855c53733fa8b596d848b
coJC <dlwhdcks34@naver.com>
2023-09-20 00:20:46 +0900

    BOJ STEP7 2563 STEP7 FINISH

--- src/boj/step7/B2563.java
+++ src/boj/step7/B2563.java
@@ -4,35 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*
+* 다시 풀어보기
+* */
 public class B2563 {
     static int N;
-    static int[][] arr;
-
-    static void input() throws IOException {
+    static boolean[][] arr;
+    static int total;
+    static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
-        arr = new int[N][2];
+        arr = new boolean[101][101];
+        total = 0;
 
-        for (int i=0;i<arr.length;i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j=0;j<arr[i].length;j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        total++;
+                    }
+                }
             }
         }
+
+        System.out.println(total);
     }
 
     public static void main(String[] args) throws IOException {
-        input();
         solution();
-    }
-
-    static void solution() {
-        for (int i=0;i<arr.length;i++) {
-            for (int j=0;j<arr[i].length;j++) {
-                System.out.println(arr[i][j]);
-            }
-        }
     }
 }
